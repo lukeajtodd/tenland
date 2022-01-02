6bf9ab694b7e85c75545023cf7a6b4169ebea3ca
Luke Todd <luketodd@zoho.com>
2022-01-02 22:08:23 +0000

    Include WindiCSS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import WindiCSSWebpackPlugin from "windicss-webpack-plugin"
+
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
 }
