cb8a49c264cd73bfd14e79dae242a93bfcacca66
Luke Todd <luketodd@zoho.com>
2022-01-04 13:25:07 +0000

    Font and positioning updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,17 @@ import Footer from '../components/Footer'
 const Home: NextPage = () => {
   return (
     <>
-      <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row justify-center items-center">
-        <div className="flex flex-col w-full xl:w-2/5 justify-center overflow-y-hidden px-3">
-          <h1 className="my-4 text-3xl md:text-6xl text-body font-bold leading-tight text-left">
+      <div className="container pt-24 md:pt-36 lg:pt-12 2xl:pt-24 mx-auto flex flex-wrap flex-col md:flex-row justify-center items-center">
+        <div className="w-full xl:w-4/5">
+          <h1 className="my-4 text-3xl md:text-6xl lg:text-9xl text-body font-bold leading-tight text-left">
             Time to shake up the rental space.
-            {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-              future
-            </span> */}
           </h1>
-          {/* <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
-            Don't get stuck renting forever, gain a future as you go!
-          </p> */}
-          <hr className="text-primary bg-primary h-1 w-48 my-4"/>
-          <Newsletter />
+          <hr className="text-primary bg-primary h-1 w-48 my-4 lg:h-2 lg:mt-24" />
+        </div>
+        <div className="w-full xl:w-4/5 flex justify-center lg:justify-end overflow-y-hidden px-3">
+          <div className="w-full lg:w-3/5 xl:w-2/4">
+            <Newsletter />
+          </div>
         </div>
         {/* <Footer /> */}
       </div>
